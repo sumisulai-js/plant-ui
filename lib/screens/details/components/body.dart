814f009e0f7011ae0ca277171bdd35e0aca59d41
Sumi Sulai <ssumi@netapp.com>
2024-03-15 00:39:52 +0530

    all

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+
+import 'image_and_icons.dart';
+import 'title_and_price.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(size: size),
+          TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
